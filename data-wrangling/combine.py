651e5c19d701cfeba3d2478c8a5742f33f811a81
Jerry Yang <j.yang5@elsevier.com>
2024-11-25 12:04:02 +0000

    explore a little

--- data-wrangling/combine.py
+++ data-wrangling/combine.py
@@ -2,25 +2,29 @@ from extract import expanded_customers, expanded_products, expanded_sales
 
 def combine_data():
     sales = expanded_sales()
+    print("sales: \n", sales.head())
     customers = expanded_customers()
     products = expanded_products()
     # merge sales and customers
     sales_customers = sales.merge(customers, how='left', left_on='CustomerID', right_on='CustomerID')
+    print("sales_customers: \n", sales_customers.head())
     # merge sales_customers and products
     sales_customers_products = sales_customers.merge(products, how='left', left_on='ProductID', right_on='ProductID')
+    # print("sales_customers_products: \n", sales_customers_products.head())
     return sales_customers_products
 
-def clean_combined_data():
-    data = combine_data()
-    # data types
-    print(data.describe())
-    # missing values / nulls
-    print(data.isnull().sum())
-    # duplicates
-    print(data.duplicated().sum())
-    # return data
+# def clean_combined_data():
+#     data = combine_data()
+#     # data types
+#     print(data.describe())
+#     # missing values / nulls
+#     print(data.isnull().sum())
+#     # duplicates
+#     print(data.duplicated().sum())
+#     # return data
 
 if __name__ == "__main__":
-    print(combine_data().head())
-    print(combine_data().shape)
-    clean_combined_data()
+    combined_data = combine_data()
+    print(combined_data.head())
+    print(combined_data.shape)
+    # clean_combined_data()
--- data-wrangling/explore.py
+++ data-wrangling/explore.py
@@ -2,6 +2,14 @@ from extract import expanded_customers, expanded_products, expanded_sales
 from combine import combine_data
 # explore the combined dataset
 
+combined_data = combine_data()
 # number of products brought from each category by each customer?
 
 
+# Question: total sales by each customer
+total_sales_by_each_customer = combined_data.groupby('CustomerID').apply(lambda x: (x['Quantity'] * x['PricePerUnit']).sum()).reset_index(name='total')
+print(total_sales_by_each_customer.sort_values(by='total', ascending=False))
+
+# Question: which category of products are most sold?
+most_sold_category = combined_data.groupby('Category').apply(lambda x: (x['Quantity'] * x['PricePerUnit']).sum()).reset_index(name='total')
+print(most_sold_category.sort_values(by='total', ascending=False))
